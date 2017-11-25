127e3f20ddc153aac19f65ec5a75d81643e9774a
zhendoubi <17855593696@163.com>
2017-11-25 19:43:41 +0800

    Default

--- test1.py
+++ test1.py
@@ -1,13 +1,13 @@
 # _*_ coding:utf-8 _*_
-#hehe = '我爱你'
-#answer = input('最想听的一句话：')
 
-#while True:
-  #  if answer == hehe:
- #       break
- #   answer = input('抱歉，错啦，请重新输入：')
+hehe = '我爱你'
+answer = input('最想听的一句话：')
+while True:
+  if answer == hehe:
+       break
+answer = input('抱歉，错啦，请重新输入：')
 
-#print('哎呦，不错呦')
+print('哎呦，不错呦')
 
 for i in  range(10):
     if i%2 !=0:
--- 函数.py
+++ 函数.py
@@ -1,3 +1,56 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
+# 定义一个函数
+def MyFirstFunction():
+    print('这是我创建的第一个函数！很不错，继续加油！')
+
+MyFirstFunction()
+
+
+def MySecondFunction(name):
+    print(name + '，我爱你')
+
+MySecondFunction('AHUT')
+
+
+def add(n1, n2):
+    result = n1 + n2
+    print(result)
+
+add(1, 2)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+print(add(1, 2))
+
+# 关键字参数
+def SaySome(name, words):
+    print(name + '->' + words)
+
+SaySome('AHUT', 'CS')
+SaySome(words='AHUT', name='CS')
+
+
+def SaySome(name='AHUT', words='CS'):
+    print(name + '->' + words)
+
+SaySome()
+SaySome('HeHe')
+
+# 收集(可变)参数
+def test(*params):
+    print('参数的长度是:', len(params));
+    print('第二个参数是:', params[1])
+
+test('AHUT', 'CS')
+
+def hello():
+    print('Hello world!')
+
+temp = hello()
+print('temp =', temp)
+
+# 函数变量的作用域问题
