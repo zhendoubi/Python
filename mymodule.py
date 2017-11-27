e3278bc1fa970c71b166ad6238d2d9c4212ff9ef
zhendoubi <17855593696@163.com>
2017-11-27 16:23:24 +0800

    Default

---
+++ mymodule.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+
+def sayhi():
+    print('Hi!')
+
+version = '0.1'
--- 模块.py
+++ 模块.py
@@ -8,3 +8,27 @@ for i in sys.argv:
     print(i)
 
 print('\n\nThe PYTHONPATH is', sys.path, '\n')
+
+# from ... import 语句
+# from sys import *
+# from ... import **
+
+
+# 模块的__name__
+# 使用模块的__name__
+if __name__ == '__main__':
+    print('AHUT')
+else:
+    print('CS')
+
+# 这个模块应该被放置在我们输入它的程序的同一个目录中
+import mymodule
+
+mymodule.sayhi()
+print('Version',mymodule.version)
+
+# 使用from...import
+from mymodule import sayhi,version
+
+sayhi()
+print('Version', version)
