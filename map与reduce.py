7cae8694df71a282829a9d2548d90701976feeeb
zhendoubi <17855593696@163.com>
2017-12-03 22:01:50 +0800

    Default

--- map与reduce.py
+++ map与reduce.py
@@ -37,3 +37,29 @@ def char2num(s):
 
 
 print(reduce(fn, map(char2num, '17259')))
+
+
+# 利用map()函数，把用户输入的不规范的英文名字，变为首字母大写，其他小写的规范名字。
+# 输入：['adam', 'LISA', 'barT']，输出：['Adam', 'Lisa', 'Bart']
+def normalize(name):
+    return name[0].upper() + name[1:].lower()
+
+
+L1 = ['adam', 'LISA', 'barT']
+L2 = list(map(normalize, L1))
+print(L2)
+
+
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积.
+def prod(L):
+    return reduce(lambda x, y: x * y, L)
+
+
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+
+
+# 利用map和reduce编写一个str2float函数，把字符串'123.456'转换成浮点数123.456
